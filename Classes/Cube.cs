196f8b81a153076a60e56339e4a37d3834ab69e7
Andrey <andrey.kozhevnikov@devexpress.com>
2020-10-10 09:49:02 +0300

    init data

---
+++ Classes/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver.Classes {
+    /*Legend
+     * Red - 1
+     * Green - 10
+     * Blue - 100
+     * Yellow - 1000
+     */
+
+    public class Cube {
+        public Cube(int[] sides) {
+            Sides = sides;
+            Lines = CreateLines();
+        }
+
+        int[][] CreateLines() {
+            var l0= new int[] { Sides[0], Sides[1], Sides[2], Sides[3] };
+            var l1= new int[] { Sides[0], Sides[5], Sides[2], Sides[6] };
+            var l2= new int[] { Sides[2], Sides[5], Sides[3], Sides[6] };
+            var res = new int[][] { l0, l1, l2 };
+            return res;
+        }
+
+        public int[] Sides { get; set; }
+        public int[][] Lines{ get; set; }
+       
+    }
+}
---
+++ Classes/Solver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver.Classes {
+    /*Legend
+   * Red - 1
+   * Green - 10
+   * Blue - 100
+   * Yellow - 1000
+   */
+    public class Solver {
+
+       Cube[]  PrepareCubes() {
+            var c1 = new Cube(new int[]{1,1000,100,10,1,10 });
+            var c2 = new Cube(new int[]{1,1000,10,100,1,1000 });
+            var c3 = new Cube(new int[]{1,10,100,1000,1000,1000});
+            var c4 = new Cube(new int[]{1,1000,100,100,10,10});
+
+            var res = new Cube[] { c1, c2, c3, c4 };
+            return res;
+        }
+
+        public void Solve() {
+            var cubes = PrepareCubes();
+
+            for(int i = 0; i < 4; i++) {
+                
+            }
+
+        }
+
+    }
+}
